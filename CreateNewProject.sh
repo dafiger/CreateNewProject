635825114bc915eb9598e52d5b63d3af6e2b5cbe
wangpengfei <wangpengfei@purang.com>
2019-11-29 17:11:20 +0800

    Update CreateNewProject.sh

--- CreateNewProject.sh
+++ CreateNewProject.sh
@@ -1,107 +1,94 @@
 #!/bin/bash
-# chmod 755 CreateNewModule.sh
 
-# 定义颜色
-HighLightColor='\033[0;36m'
-DefaultColor='\033[0;m'
+# 编码处理
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+# 颜色
+RedColor='\033[31m'
+GreenColor='\033[32m'
+YellowColor='\033[33m'
+DefaultColor='\033[0m'
+
+
+# demo名
+DemoName='OCProjectDemo'
+# demo地址
+DemoClonePath='https://github.com/dafiger/OCProjectDemo.git'
+
 
 # 工程名
 ProjectName=''
 # 组织名
 OrganizationName=''
-# 模板工程clone地址
-DemoClonePath='https://github.com/dafiger/OCProjectDemo.git'
-# 模板工程名
-DemoName='OCProjectDemo'
-
 # 作者
-Author='dafiger'
-# 作者和组织一样
-# Author="${OrganizationName}"
-
-# Spec仓库名称
-SpecName='DFSpec'
-# Spec仓库主页地址
-SpecHomePage='https://github.com/dafiger/DFSpec.git'
-
+Author=''
 # 确认状态
 ConfirmStatus='N'
 
+
 # 输入项目名
 InputProjectName() {
-    echo -e "\n"
-	read -p 'Please input your Project name: ' ProjectName
-    if 
-    	test -z ${ProjectName};
+    read -p "请输入新建项目名称: " ProjectName
+    # 字符串长度为0
+    if test -z ${ProjectName}
     then
-    	InputProjectName
+        InputProjectName
     fi
 }
 
 # 输入组织名
 InputOrganizationName() {
-    echo -e "\n"
-    read -p 'Please input your Organization name: ' OrganizationName
-    if 
-    	test -z ${OrganizationName};
+    read -p "请输入新建项目组织名称: " OrganizationName
+    # 字符串长度为0
+    if test -z ${OrganizationName}
     then
-    	InputOrganizationName
+        InputOrganizationName
     fi
-    # 作者和组织一样
-    Author="${OrganizationName}"
+    # 组织名和作者一样
+    Author=${OrganizationName}
 }
 
-# 审查输入内容
+# 检验输入内容
 CheckInfomation() {
     InputProjectName
     InputOrganizationName
 
-    echo -e "\n================================================"
-    echo -e "  Project Name        :  ${HighLightColor}${ProjectName}${DefaultColor}"
-    echo -e "  Organization Name   :  ${HighLightColor}${OrganizationName}${DefaultColor}"
-    echo -e "  Author              :  ${HighLightColor}${Author}${DefaultColor}"
-    echo -e "  Spec Name           :  ${HighLightColor}${SpecName}${DefaultColor}"
-    echo -e "  Spec HomePage       :  ${HighLightColor}${SpecHomePage}${DefaultColor}"
+    echo -e "\n${DefaultColor}================================================"
+    echo -e "  新建项目名称：${RedColor}${ProjectName}${DefaultColor}"
+    echo -e "  新建项目组织名称：${RedColor}${OrganizationName}${DefaultColor}"
+    echo -e "  新建项目作者：${RedColor}${OrganizationName}${DefaultColor}"
     echo -e "================================================\n"
 }
 
-# 循环检查
-while [ ${ConfirmStatus} != 'y' -a ${ConfirmStatus} != 'Y' ]
+# 循环检验
+while [ ${ConfirmStatus} != "y" -a ${ConfirmStatus} != "Y" ] 
 do
-    if 
-        [ ${ConfirmStatus} == 'n' -o ${ConfirmStatus} == 'N' ]; 
-    then
+    if [ ${ConfirmStatus} == "n" -o ${ConfirmStatus} == "N" ]
+    then 
         CheckInfomation
     fi
-    read -p 'Are you sure? (y/n):' ConfirmStatus
+    read -p "确定? (y/n):" ConfirmStatus
 done
 
-# 返回桌面
-cd
-cd Desktop/
-
 # 桌面新建文件夹
-# mkdir ${ProjectName}
-# date_now=`date +%Y%m%d`
-date_now=`date +%Y%m%d%H%M%S`
-mkdir -p ${ProjectName}/${date_now}
-cd ${ProjectName}/${date_now}
+cd ~/Desktop
+ProjectPath="Demo$(date +%y%m%d%H%M%S)"
+mkdir -p ${ProjectPath}
+cd ${ProjectPath}
 
-# 下载模板工程
+# 下载demo
 git clone ${DemoClonePath}
-# ls
 
-# 复制主工程
-# 递归拷贝
-# cp -r "${DemoName}" "${ProjectName}"
-# 直接拷贝不需要询问
-# cp -f "${DemoName}" "${ProjectName}"
-# 主工程重新命名
-mv "${DemoName}" "${ProjectName}"
+# 项目重命名
+mv ${DemoName} ${ProjectName}
+
 # 打开工程
 cd ${ProjectName}
-mv "${DemoName}" "${ProjectName}"
-mv "${DemoName}.xcodeproj" "${ProjectName}.xcodeproj"
+mv ${DemoName} ${ProjectName}
+mv ${DemoName}.xcodeproj ${ProjectName}.xcodeproj
+
+exit 0
 
 # 文件路径
 license_Path="./LICENSE"
